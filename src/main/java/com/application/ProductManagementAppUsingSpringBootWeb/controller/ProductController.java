d1854965054940177dfc14a819b21888ee5bdb44
imprathamraj <rjpratham05@gmail.com>
2025-03-27 01:25:11 +0530

    [Pratham Raj] Enable. Cross Origin Requests in ProductController to allow Angular frontend access

--- src/main/java/com/application/ProductManagementAppUsingSpringBootWeb/controller/ProductController.java
+++ src/main/java/com/application/ProductManagementAppUsingSpringBootWeb/controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     @Autowired
