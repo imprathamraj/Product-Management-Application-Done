c8710cc161b46810b82329ee239506aad541d89f
imprathamraj <rjpratham05@gmail.com>
2025-04-01 17:59:34 +0530

    [Pratham Raj] Add. User.java class in entity layer with user id, username, and password

---
+++ src/main/java/com/application/ProductManagementAppUsingSpringBootWeb/entity/User.java
@@ -0,0 +1,24 @@
+package com.application.ProductManagementAppUsingSpringBootWeb.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enabled = true;
+
+}
