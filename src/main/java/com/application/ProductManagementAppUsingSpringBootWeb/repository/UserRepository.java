a156d967f7f4be8123e107f31332938513324d64
imprathamraj <rjpratham05@gmail.com>
2025-04-01 18:00:43 +0530

    [Pratham Raj] Add. UserRepository interface in repository layer with Optional<User> findByUsername(String username) method

---
+++ src/main/java/com/application/ProductManagementAppUsingSpringBootWeb/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.application.ProductManagementAppUsingSpringBootWeb.repository;
+
+import com.application.ProductManagementAppUsingSpringBootWeb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
