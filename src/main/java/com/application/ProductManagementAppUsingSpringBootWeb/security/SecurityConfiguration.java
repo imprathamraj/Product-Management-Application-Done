f59814aa2253cff9a4207708db155f94c80742c1
imprathamraj <rjpratham05@gmail.com>
2025-04-01 16:25:08 +0530

    [Pratham Raj] Modify. Using default username and password form UserDetailService and InMemoryUserDetailsManager

--- src/main/java/com/application/ProductManagementAppUsingSpringBootWeb/security/SecurityConfiguration.java
+++ src/main/java/com/application/ProductManagementAppUsingSpringBootWeb/security/SecurityConfiguration.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -34,6 +38,19 @@ public class SecurityConfiguration {
                 // To make stateless
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .build();
+    }
+
+    // Using default username and password form UserDetailService and InMemoryUserDetailsManager
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails u1 = User
+                .withDefaultPasswordEncoder()
+                .username("u1")
+                .password("u1")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(u1);
     }
 
 
